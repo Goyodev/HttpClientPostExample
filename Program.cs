609240e1f3660147efd5072e55ac1289fbe3a5b4
goyo <gregorio.escriche.dosa@gmail.com>
2024-05-09 19:45:48 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+string url = "https://petstore.swagger.io/v2/pet";
+
+string jsonBody = "{\"id\": 0, \"name\": \"doggie\"}";
+
+using (HttpClient client = new HttpClient())
+{
+    var request = new HttpRequestMessage
+    {
+        Method = HttpMethod.Post,
+        RequestUri = new Uri(url),
+        Content = new StringContent(jsonBody, Encoding.UTF8, "application/json")
+    };
+
+    HttpResponseMessage response = await client.SendAsync(request);
+
+    if (response.IsSuccessStatusCode)
+    {
+        string responseBody = await response.Content.ReadAsStringAsync();
+        Console.WriteLine("Respuesta:");
+        Console.WriteLine(responseBody);
+    }
+    else
+    {
+        Console.WriteLine($"La solicitud no fue exitosa. Código de estado: {response.StatusCode}");
+    }
+}
